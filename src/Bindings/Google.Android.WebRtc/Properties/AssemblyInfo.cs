a88ec3098bdd87a0b20a9d62e9429e319fb8224f
Daniel Gatto <d.mario.gatto@gmail.com>
2020-05-16 08:53:21 +0930

    added nuspec for droid

--- src/Bindings/Google.Android.WebRtc/Properties/AssemblyInfo.cs
+++ src/Bindings/Google.Android.WebRtc/Properties/AssemblyInfo.cs
@@ -1,26 +1,26 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using Android.App;
+﻿//using System.Reflection;
+//using System.Runtime.CompilerServices;
+//using System.Runtime.InteropServices;
+//using Android.App;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Xamarin.Google.WebRTC")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Xamarin.Google.WebRTC")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
+//// General Information about an assembly is controlled through the following 
+//// set of attributes. Change these attribute values to modify the information
+//// associated with an assembly.
+//[assembly: AssemblyTitle("Xamarin.Google.WebRTC")]
+//[assembly: AssemblyDescription("Xamarin Bindings for Google WebRTC")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("Xamarin.Google.WebRTC")]
+//[assembly: AssemblyCopyright("Copyright ©  2020")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+//[assembly: ComVisible(false)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//// Version information for an assembly consists of the following four values:
+////
+////      Major Version
+////      Minor Version 
+////      Build Number
+////      Revision
+//[assembly: AssemblyVersion("1.0.30039")]
+//[assembly: AssemblyFileVersion("1.0.30039")]
